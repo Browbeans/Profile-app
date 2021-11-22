c797a28607b3aab6bae5850a65f5a67d7e2f07d4
Oliver Johansson <oliverjohansson3@gmail.com>
2021-11-22 21:15:53 +0100

    Added qualification section

---
+++ src/Components/QualificationsCmp/Backend.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Backend:React.FC = () => {
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default Backend;
---
+++ src/Style/BackendStyle.ts
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core";
+
+const backendStyle = makeStyles(
+  () => ({
+
+  }),
+  {index: 1}
+)
+
+export default backendStyle;
